a29a01080c4a3d96eb1093a5f8bb63775b7eb642
TrangDuLam <andyhardaway07@gmail.com>
2023-04-29 11:33:41 +0800

    master

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='audioviz',
     packages=['audioviz'],
-    version='0.1.3',
+    version='0.1.4',
     description='An user-friendly music information retrieval tools interfacing with Google Colab',
 
     url='https://github.com/TrangDuLam/audioviz',
